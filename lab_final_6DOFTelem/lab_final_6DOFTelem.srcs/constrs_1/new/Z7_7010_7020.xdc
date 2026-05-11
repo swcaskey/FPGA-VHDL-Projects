910e7989a02c37c065269c20b4362dea3e504ccc
Seth Caskey <swcaskey@gmail.com>
2026-05-11 14:00:18 -0400

    LEDs

--- lab_final_6DOFTelem/lab_final_6DOFTelem.srcs/constrs_1/new/Z7_7010_7020.xdc
+++ lab_final_6DOFTelem/lab_final_6DOFTelem.srcs/constrs_1/new/Z7_7010_7020.xdc
@@ -1,9 +1,12 @@
 ## Clock signal
-set_property -dict { PACKAGE_PIN L16   IOSTANDARD LVCMOS33 } [get_ports { sysclk }];
+set_property -dict { PACKAGE_PIN K17   IOSTANDARD LVCMOS33 } [get_ports { sysclk }];
 create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports { sysclk }];
 
 ## Hardware Reset Button (BTN0)
-set_property -dict { PACKAGE_PIN R18   IOSTANDARD LVCMOS33 } [get_ports { btn[0] }];
+set_property -dict { PACKAGE_PIN K18   IOSTANDARD LVCMOS33 } [get_ports { btn[0] }];
+
+## Mode Select Switch (SW0)
+set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 } [get_ports { sw[0] }];
 
 ## Diagnostic LEDs
 set_property -dict { PACKAGE_PIN M14   IOSTANDARD LVCMOS33 } [get_ports { led[0] }];
@@ -11,23 +14,13 @@ set_property -dict { PACKAGE_PIN M15   IOSTANDARD LVCMOS33 } [get_ports { led[1]
 set_property -dict { PACKAGE_PIN G14   IOSTANDARD LVCMOS33 } [get_ports { led[2] }];
 set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { led[3] }];
 
-## HDMI TX (Video Output)
-set_property -dict { PACKAGE_PIN H17   IOSTANDARD TMDS_33  } [get_ports { TMDS_Clk_n }];
-set_property -dict { PACKAGE_PIN H16   IOSTANDARD TMDS_33  } [get_ports { TMDS_Clk_p }];
-set_property -dict { PACKAGE_PIN D20   IOSTANDARD TMDS_33  } [get_ports { TMDS_Data_n[0] }];
-set_property -dict { PACKAGE_PIN D19   IOSTANDARD TMDS_33  } [get_ports { TMDS_Data_p[0] }];
-set_property -dict { PACKAGE_PIN B20   IOSTANDARD TMDS_33  } [get_ports { TMDS_Data_n[1] }];
-set_property -dict { PACKAGE_PIN C20   IOSTANDARD TMDS_33  } [get_ports { TMDS_Data_p[1] }];
-set_property -dict { PACKAGE_PIN A20   IOSTANDARD TMDS_33  } [get_ports { TMDS_Data_n[2] }];
-set_property -dict { PACKAGE_PIN B19   IOSTANDARD TMDS_33  } [get_ports { TMDS_Data_p[2] }];
-
-## Pmod Header JD (PMOD NAV - Plug into all 12 pins of JD)
+## Pmod Header JD (PMOD NAV)
 set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33 } [get_ports { nav_cs }];
 set_property -dict { PACKAGE_PIN T15   IOSTANDARD LVCMOS33 } [get_ports { nav_mosi }];
 set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33 } [get_ports { nav_miso }];
 set_property -dict { PACKAGE_PIN R14   IOSTANDARD LVCMOS33 } [get_ports { nav_sclk }];
 
-## Pmod Header JC (PMOD JSTK2 - Plug ONLY into the TOP ROW of JC)
+## Pmod Header JC (PMOD JSTK2, top row)
 set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports { jstk_cs }];
 set_property -dict { PACKAGE_PIN W15   IOSTANDARD LVCMOS33 } [get_ports { jstk_mosi }];
 set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { jstk_miso }];
--- lab_final_6DOFTelem/lab_final_6DOFTelem.srcs/sources_1/new/hdmi_telemetry_top.vhd
+++ lab_final_6DOFTelem/lab_final_6DOFTelem.srcs/sources_1/new/hdmi_telemetry_top.vhd
@@ -4,105 +4,54 @@ use ieee.numeric_std.all;
 
 entity hdmi_telemetry_top is
     port (
-        -- Board Clock
-        sysclk     : in  std_logic; -- 125 MHz physical board clock
-        btn        : in  std_logic_vector(0 downto 0); -- Hardware reset button
-        
+        -- Board clock/reset/select
+        sysclk     : in  std_logic; -- 125 MHz
+        btn        : in  std_logic_vector(0 downto 0);
+        sw         : in  std_logic_vector(0 downto 0); -- SW0 mode select
+
         -- SPI Lines for PMOD NAV
         nav_mosi   : out std_logic;
         nav_miso   : in  std_logic;
         nav_sclk   : out std_logic;
         nav_cs     : out std_logic;
-        
+
         -- SPI Lines for PMOD JSTK2
         jstk_mosi  : out std_logic;
         jstk_miso  : in  std_logic;
         jstk_sclk  : out std_logic;
         jstk_cs    : out std_logic;
-        
-        -- HDMI TMDS Outputs
-        TMDS_Clk_p : out std_logic;
-        TMDS_Clk_n : out std_logic;
-        TMDS_Data_p: out std_logic_vector(2 downto 0);
-        TMDS_Data_n: out std_logic_vector(2 downto 0);
-        
+
         -- Diagnostic LEDs
         led        : out std_logic_vector(3 downto 0)
     );
 end hdmi_telemetry_top;
 
 architecture structural of hdmi_telemetry_top is
+    signal rst : std_logic;
+    signal spi_en : std_logic;
 
-    -- Reset signals
-    signal rst       : std_logic;
-    signal rst_n     : std_logic;
-    signal vid_reset : std_logic;
-    
-    -- Clock signals
-    signal pclk      : std_logic; -- 25 MHz from clk_wiz
-    signal serial_clk: std_logic; -- Phase-locked 125 MHz from clk_wiz
-    signal clk_lock  : std_logic;
-    signal spi_en    : std_logic; -- 1 MHz pulse
-    
-    -- SPI Master shared buses
+    -- NAV SPI bus and outputs
     signal spi1_start, spi1_ready : std_logic;
     signal spi1_din, spi1_dout    : std_logic_vector(7 downto 0);
-    
+    signal pitch_val, roll_val, yaw_val : std_logic_vector(15 downto 0);
+    signal imu_start, imu_valid : std_logic;
+
+    -- JSTK SPI bus and outputs
     signal spi2_start, spi2_ready : std_logic;
     signal spi2_din, spi2_dout    : std_logic_vector(7 downto 0);
-    
-    -- Sensor Data
-    signal pitch_val, roll_val, yaw_val : std_logic_vector(15 downto 0);
-    signal jstk_x_val, jstk_y_val       : std_logic_vector(15 downto 0);
-    signal btn_center                   : std_logic;
-    
-    -- FSM Handshakes
-    signal imu_start, imu_valid   : std_logic;
+    signal jstk_x_val, jstk_y_val : std_logic_vector(15 downto 0);
+    signal btn_center             : std_logic;
     signal jstk_start, jstk_valid : std_logic;
-    signal math_start, math_done  : std_logic;
-    
-    -- Math to Renderer Data
-    signal v0_x, v0_y : std_logic_vector(9 downto 0);
-    signal v1_x, v1_y : std_logic_vector(9 downto 0);
-    signal v2_x, v2_y : std_logic_vector(9 downto 0);
-    signal v3_x, v3_y : std_logic_vector(9 downto 0);
-    
-    -- VGA Timing to Renderer & HDMI
-    signal hsync, vsync, vde : std_logic;
-    signal hcount, vcount    : std_logic_vector(9 downto 0);
-    
-    -- Video RGB Data
-    signal red_data, green_data, blue_data : std_logic_vector(7 downto 0);
-    signal rgb_combined : std_logic_vector(23 downto 0);
 
-    -- LED pulse stretchers for visible heartbeat diagnostics
-    constant LED_HOLD_TICKS : unsigned(23 downto 0) := to_unsigned(12500000, 24); -- ~100 ms @125 MHz
-    constant LED_ZERO       : unsigned(23 downto 0) := to_unsigned(0, 24);
-    signal imu_led_cnt, jstk_led_cnt, math_led_cnt : unsigned(23 downto 0) := (others => '0');
+    constant IMU_THRESH_HI : signed(15 downto 0) := to_signed(2000, 16);
+    constant IMU_THRESH_LO : signed(15 downto 0) := to_signed(-2000, 16);
+    constant JSTK_THRESH_HI : unsigned(15 downto 0) := to_unsigned(160, 16); -- 128 + 32
+    constant JSTK_THRESH_LO : unsigned(15 downto 0) := to_unsigned(96, 16);  -- 128 - 32
 
-    -- LED 3 watchdog: assert only if no subsystem handshake activity for ~1 second
-    constant ACTIVITY_TIMEOUT : unsigned(26 downto 0) := to_unsigned(125000000, 27);
-    signal activity_cnt : unsigned(26 downto 0) := (others => '0');
-
+    signal imu_led0, imu_led1, imu_led2 : std_logic;
+    signal jstk_led0, jstk_led1 : std_logic;
 begin
-
-    -- Force reset inactive for bring-up/debug to avoid bad BTN pin mapping holding MMCM reset.
-    rst <= '0';
-    rst_n <= '1';
-    vid_reset <= not clk_lock;
-    rgb_combined <= red_data & green_data & blue_data;
-
-    --------------------------------------------------------------------------
-    -- CLOCKING INFRASTRUCTURE
-    --------------------------------------------------------------------------
-    U_CLK_WIZ : entity work.clk_wiz_0
-        port map (
-            clk_in1  => sysclk,
-            reset    => rst,
-            clk_out1 => pclk,
-            clk_out2 => serial_clk, -- Routed for the HDMI IP block
-            locked   => clk_lock
-        );
+    rst <= btn(0);
 
     U_SPI_CLK : entity work.spi_clock_en
         port map (
@@ -110,21 +59,6 @@ begin
             spi_en_1mhz => spi_en
         );
 
-    --------------------------------------------------------------------------
-    -- CENTRAL CONTROLLER
-    --------------------------------------------------------------------------
-    U_MASTER_FSM : entity work.central_fsmd
-        port map (
-            clk => sysclk, rst => rst,
-            video_active => vde,
-            imu_start => imu_start, imu_valid => imu_valid,
-            jstk_start => jstk_start, jstk_valid => jstk_valid,
-            math_start => math_start, math_done => math_done
-        );
-
-    --------------------------------------------------------------------------
-    -- IMU SENSOR PIPELINE
-    --------------------------------------------------------------------------
     U_SPI_NAV : entity work.spi_master
         port map (
             clk => sysclk, spi_en => spi_en, rst => rst,
@@ -143,9 +77,6 @@ begin
             cs_ag => nav_cs
         );
 
-    --------------------------------------------------------------------------
-    -- JOYSTICK SENSOR PIPELINE
-    --------------------------------------------------------------------------
     U_SPI_JSTK : entity work.spi_master
         port map (
             clk => sysclk, spi_en => spi_en, rst => rst,
@@ -164,97 +95,40 @@ begin
             cs_jstk => jstk_cs
         );
 
-    --------------------------------------------------------------------------
-    -- 3D MATHEMATICS DATAPATH
-    --------------------------------------------------------------------------
-    U_MATH : entity work.math_pipeline
-        port map (
-            clk => sysclk, rst => rst, start => math_start,
-            pitch => pitch_val, roll => roll_val, yaw => yaw_val,
-            jstk_x => jstk_x_val, jstk_y => jstk_y_val,
-            v0_x => v0_x, v0_y => v0_y, v1_x => v1_x, v1_y => v1_y,
-            v2_x => v2_x, v2_y => v2_y, v3_x => v3_x, v3_y => v3_y,
-            done => math_done
-        );
-
-    --------------------------------------------------------------------------
-    -- VIDEO RENDERING & OUTPUT
-    --------------------------------------------------------------------------
-    U_VGA_CTRL : entity work.vga_ctrl
-        port map (
-            clk => pclk, en => '1',
-            vid => vde, hs => hsync, vs => vsync,
-            hc => hcount, vc => vcount
-        );
-
-    U_RENDERER : entity work.wireframe_renderer
-        port map (
-            pclk => pclk, rst => rst, video_active => vde,
-            hcount => hcount, vcount => vcount,
-            v0_x => v0_x, v0_y => v0_y, v1_x => v1_x, v1_y => v1_y,
-            v2_x => v2_x, v2_y => v2_y, v3_x => v3_x, v3_y => v3_y,
-            red => red_data, green => green_data, blue => blue_data
-        );
-
-    U_RGB2DVI : entity work.rgb2dvi_0
-        port map (
-            TMDS_Clk_p  => TMDS_Clk_p,
-            TMDS_Clk_n  => TMDS_Clk_n,
-            TMDS_Data_p => TMDS_Data_p,
-            TMDS_Data_n => TMDS_Data_n,
-            aRst        => vid_reset, 
-            vid_pData   => rgb_combined,
-            vid_pVDE    => vde,
-            vid_pHSync  => hsync,
-            vid_pVSync  => vsync,
-            PixelClk    => pclk,
-            SerialClk   => serial_clk -- Must be phase-related to PixelClk from same clk_wiz/MMCM
-        );
-
+    -- Autonomous repeated reads for each sensor controller
     process(sysclk)
     begin
         if rising_edge(sysclk) then
             if rst = '1' then
-                imu_led_cnt <= (others => '0');
-                jstk_led_cnt <= (others => '0');
-                math_led_cnt <= (others => '0');
-                activity_cnt <= (others => '0');
+                imu_start <= '0';
+                jstk_start <= '0';
             else
-                if imu_valid = '1' then
-                    imu_led_cnt <= LED_HOLD_TICKS;
-                elsif imu_led_cnt /= LED_ZERO then
-                    imu_led_cnt <= imu_led_cnt - 1;
+                if imu_start = '0' then
+                    imu_start <= '1';
+                elsif imu_valid = '1' then
+                    imu_start <= '0';
                 end if;
 
-                if jstk_valid = '1' then
-                    jstk_led_cnt <= LED_HOLD_TICKS;
-                elsif jstk_led_cnt /= LED_ZERO then
-                    jstk_led_cnt <= jstk_led_cnt - 1;
-                end if;
-
-                if math_done = '1' then
-                    math_led_cnt <= LED_HOLD_TICKS;
-                elsif math_led_cnt /= LED_ZERO then
-                    math_led_cnt <= math_led_cnt - 1;
-                end if;
-
-                if (imu_valid = '1') or (jstk_valid = '1') or (math_done = '1') then
-                    activity_cnt <= (others => '0');
-                elsif activity_cnt < ACTIVITY_TIMEOUT then
-                    activity_cnt <= activity_cnt + 1;
+                if jstk_start = '0' then
+                    jstk_start <= '1';
+                elsif jstk_valid = '1' then
+                    jstk_start <= '0';
                 end if;
             end if;
         end if;
     end process;
-    
-    -- LED 0/1/2: stretched heartbeats for human-visible diagnostics
-    led(0) <= '1' when imu_led_cnt /= LED_ZERO else '0';
-    led(1) <= '1' when jstk_led_cnt /= LED_ZERO else '0';
-    led(2) <= '1' when math_led_cnt /= LED_ZERO else '0';
-    
-    -- LED 3: immediate clock-lock fault, otherwise no-activity watchdog
-    led(3) <= '1' when clk_lock = '0' else
-              '1' when activity_cnt = ACTIVITY_TIMEOUT else
-              '0';
 
+    -- Mode 0 (SW0=0): NAV gyro thresholds
+    imu_led0 <= '1' when (signed(yaw_val) > IMU_THRESH_HI) or (signed(yaw_val) < IMU_THRESH_LO) else '0';
+    imu_led1 <= '1' when (signed(roll_val) > IMU_THRESH_HI) or (signed(roll_val) < IMU_THRESH_LO) else '0';
+    imu_led2 <= '1' when (signed(pitch_val) > IMU_THRESH_HI) or (signed(pitch_val) < IMU_THRESH_LO) else '0';
+
+    -- Mode 1 (SW0=1): JSTK thresholds
+    jstk_led0 <= '1' when (unsigned(jstk_x_val) > JSTK_THRESH_HI) or (unsigned(jstk_x_val) < JSTK_THRESH_LO) else '0';
+    jstk_led1 <= '1' when (unsigned(jstk_y_val) > JSTK_THRESH_HI) or (unsigned(jstk_y_val) < JSTK_THRESH_LO) else '0';
+
+    led(0) <= imu_led0 when sw(0) = '0' else jstk_led0;
+    led(1) <= imu_led1 when sw(0) = '0' else jstk_led1;
+    led(2) <= imu_led2 when sw(0) = '0' else '0';
+    led(3) <= '0';
 end structural;
